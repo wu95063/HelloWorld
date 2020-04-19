4f0a486a0338e26a62efbfaa07e49c4eab8f24dc
mrchen <892865894@qq.com>
2020-04-19 16:53:27 +0800

    新py文件

---
+++ About Print.py
@@ -0,0 +1,15 @@
+#Print意味着输出,形式：print()
+print(0)#整数形->0
+print(00000)#同样是整数形->0
+print(123456)#也是整数形->123456
+#大家可以试一下print(0123456)看看会出现什么样的结果。
+print('你是我的小苹果')#字符串->你是我的小苹果
+print("你是我的小苹果2")#字符串#包在‘ ’或者“ ”里面的都是字符串->你是我的小苹果2
+print('O----')#字符串->O----
+print(' //// ')#字符串->////
+print('love'*10)#字符串*数字=字符串->lovelovelovelovelovelovelovelovelovelove
+print(10*'love')#数字*字符串=字符串->lovelovelovelovelovelovelovelovelovelove
+#考虑print('10'*'love')会出现什么结果
+print('10*10')##字符串->10*10
+
+
