b6e39ff4e0bd157a76e4e94c622439b2b6a53093
mrchen <892865894@qq.com>
2020-04-19 22:26:33 +0800

    关于Variables.

---
+++ Variables.py
@@ -0,0 +1,26 @@
+#Variables是变量的意思。
+number=10#number是一个变量
+         #number被赋予了10这样一个数值；
+         #或者可以说10这个数值被贴上了number这样的一个标签。
+         #此时number的数据类型是整数型（int）
+print(number)#->10
+word='Hello'#同样string是一个变量
+           #'Hello'这个字符串，被贴上了word这样一个标签
+           #此时的word的数据类型是一个字符串（string）
+print(word)
+
+float_number=3.1415#float_number是一个变量
+                    #3.1415这个浮点型，被贴上了float_number这样一个标签
+                    #此时的float_number的数据类型是浮点型
+print(float_number)
+number_a=3
+number_b=4
+if number_a<number_b:#运用了if语句；if语句的特点是当if后面的条件为True的时候，才会进行输出。
+    print(number_a)#所以 if number_a<number_b,相当于if True(因为本身3<4).
+else:
+    print(number_b)
+
+
+
+
+
