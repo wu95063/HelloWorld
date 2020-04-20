3e590ec0d86dace68c4ff5270c9ce8266f6e6b05
Mr.Chen <40707704+wu95063@users.noreply.github.com>
2020-04-20 12:17:33 +0800

    Update About input.py

--- About input.py
+++ About input.py
@@ -8,4 +8,4 @@ age=2020-int(birth_year)#定义一个新的变量，调用int函数将birth_year
 print(age)#输出age这个变量，age这个变量为整数型
 print(f'Your age is {age}')#由于age这个变量是整数型，而your age is 是字符串，如果想要把二者直接进行拼接是无法达到的。所以我们调用format函数，↓↓
                            #来格式化字符串，format函数即为格式化的意思。
-                           #利用大括号{},age的类型是整数型，{age}={整数型}=字符串型，'your age is {age}'即为'字符串 字符串 字符串 字符串'，进而
+                           #利用大括号{},age的类型是整数型，{age}={整数型}=字符串型，'your age is {age}'即为'字符串 字符串 字符串 字符串'，进而输出这些字符串。
