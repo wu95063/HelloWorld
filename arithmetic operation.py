2f46771e01fa55c590c4a9b0df7dcd6e9d30f568
mrchen <892865894@qq.com>
2020-04-21 10:00:55 +0800

    算术运算

---
+++ arithmetic operation.py
@@ -0,0 +1,22 @@
+#arithmetic operation 算术运算。
+print(10+3)
+print(10*3)
+print(10/3)
+print(10-3)
+print(10**3)#求幂的operater
+print(10//3)#取整数的operater
+print(10%3)#取余数的operater
+#非常有用的增广赋值运算符。
+a=10
+a+=3#也就是a=a+3
+print(a)
+b=8
+b/=2
+print(int(b))
+#优先级
+c=10*5+(2*4-2**2)/2#优先级同数学运算一样。
+print(c)
+
+
+
+
